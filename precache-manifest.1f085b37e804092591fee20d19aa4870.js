c4c83c0656d845829c0e200eef3455b9856b4117
Matthew Lean <matt@mattlean.com>
2020-10-08 03:28:04 -0700

    Updates

---
+++ precache-manifest.1f085b37e804092591fee20d19aa4870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6124082914f69d8eb10ae701f2d74be",
+    "url": "/reddit-frontend/index.html"
+  },
+  {
+    "revision": "3be677e7e03306638eb8",
+    "url": "/reddit-frontend/static/css/main.4bd07259.chunk.css"
+  },
+  {
+    "revision": "95471139584950cddb13",
+    "url": "/reddit-frontend/static/js/2.fc67a037.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/reddit-frontend/static/js/2.fc67a037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be677e7e03306638eb8",
+    "url": "/reddit-frontend/static/js/main.65a6628f.chunk.js"
+  },
+  {
+    "revision": "9967b84022482bee85a4",
+    "url": "/reddit-frontend/static/js/runtime-main.b5b6b25d.js"
+  },
+  {
+    "revision": "0f9fec7017defeaa024e932b9bc8d342",
+    "url": "/reddit-frontend/static/media/space-snoo.0f9fec70.png"
+  }
+]);
